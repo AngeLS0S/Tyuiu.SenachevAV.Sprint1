2d389f58049ee305660048c46c0a777526a8d2bf
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-09-20 23:08:10 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SenachevAV.Sprint1.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using System.Security.Cryptography;
+using Tyuiu.SenachevAV.Sprint1.Task0.V16.Lib;
+
+namespace Tyuiu.SenachevAV.Sprint1.Task0.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        { 
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-7, res);
+        }
+    }
+}
---
+++ Tyuiu.SenachevAV.Sprint1.Task0.V16/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.SenachevAV.Sprint1.Task0.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Сеначев А.В. | АСОиУб-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУб-24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычесляет выражение 6/2/3+1-6/2*3           *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 6/2/3+1-6/2*3                                                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+
+        Console.ReadLine();
+    }
+}
