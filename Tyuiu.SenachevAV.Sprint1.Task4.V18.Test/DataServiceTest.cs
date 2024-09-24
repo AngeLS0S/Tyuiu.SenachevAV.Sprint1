3248ea902f6f632a326e422daeb89e9bb17f28e0
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-09-24 23:04:29 +0500

    Task4

---
+++ Tyuiu.SenachevAV.Sprint1.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.SenachevAV.Sprint1.Task4.V18.Lib;
+
+namespace Tyuiu.SenachevAV.Sprint1.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2; 
+            double y = 2;
+            double wait = 0.28;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SenachevAV.Sprint1.Task4.V18/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.SenachevAV.Sprint1.Task4.V18.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Сеначев А.В. | АСОиУб-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУб-24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+
+        Console.WriteLine("Введите значение x");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение y");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine (ds.Calculate(x, y));
+
+        Console.ReadLine();
+    }
+}
