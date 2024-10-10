f3acc24154feebcc4165760495beb9c7d1a10f5e
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-10-10 21:22:27 +0500

    Task5

--- Tyuiu.SenachevAV.Sprint1.Task5.V1.Lib/DataService.cs
+++ Tyuiu.SenachevAV.Sprint1.Task5.V1.Lib/DataService.cs
@@ -4,7 +4,7 @@ namespace Tyuiu.SenachevAV.Sprint1.Task5.V1.Lib
 {
     public class DataService : ISprint1Task5V1
     {
-        public int DistanceBetweenDots(double x2, double x1, double y2, double y1)
+        public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
         {
             int res = Convert.ToInt32(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
             return res;
